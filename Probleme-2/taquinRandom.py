311a20d573389910326b561753b584213b080823
Robin <robin.bechlem@gmail.com>
2025-01-29 08:48:15 +0100

    🫱🏻 - Init du projet

---
+++ Probleme-2/taquinRandom.py
@@ -0,0 +1,104 @@
+import random
+
+TROU  =  '0'
+nbcomb=0
+dim = 0
+
+def afficheJeu(jeu) :
+	for l in jeu :
+		for c in l :
+			print(c, end=' ')
+		print()
+	print()
+
+def initTaquin(nf) :
+	f = open(nf,"r")
+	lignes = f.readlines()
+	jeu, ref = [], []
+	global dim 
+	dim = len(lignes)//2
+	print(dim)
+	for l in lignes[:dim] :
+		c = l.split()
+		jeu.append(c[:])
+	for l in lignes[dim:] :
+		c = l.split()
+		ref.append(c[:])
+	return jeu, ref
+
+def chercher(val, ref) :
+	for i in range(len(ref)) :
+		if val in ref[i] : return i, ref[i].index(val)		
+
+def valJeu( jeu,  ref) :
+	sommedist=0
+	for i in range(len(jeu)) :
+		for j in range(len(jeu[i])) :
+			if (jeu[i][j] != ref[i][j]) :
+				y, x  = chercher(jeu[i][j], ref)
+				sommedist += abs(y-i) + abs(x-j)
+	return sommedist
+
+def meilleureConfig(lstJeu, ref) : 
+	return lstJeu[0]
+
+def pasDansListe(jeu, lstJeu) :
+	for j in lstJeu :
+		if valJeu(j[0], jeu) == 0 : return 0
+	return 1
+
+def copie_jeu(j) :
+	jeu=[]
+	for i in range(len(j)) :
+		jeu.append(j[i][:])
+	return jeu
+
+def jouer(ref, jeu) :
+	nbEssais = 0
+	global dim
+	fin = False
+	while not fin :
+		if nbEssais%10000 == 0 : print(nbEssais, end=" ")
+		dist = valJeu(jeu, ref)
+		#print(dist, end=' ')
+		if (dist == 0) :
+			print()
+			print("nombre de mouvements :", nbEssais)
+			afficheJeu(jeu)
+			exit (0)
+		
+		# chercher la position du trou 
+		mt, nt = chercher(TROU, jeu)
+		# on joue au hasard parmi toutes les possibilités d'action, sans tester si on revient en arrière ...
+
+		coup = random.randint(0,3)
+		if (mt > 0) and (coup==0)   :
+			nbEssais+=1
+			# on echange le trou et l'élément au dessus 
+			(jeu[mt][nt], jeu[mt-1][nt]) = (jeu[mt-1][nt], jeu[mt][nt])
+		if (mt < dim-1) and (coup==1) :
+			nbEssais+=1
+			# on echange le trou et l'élément au dessus 
+			(jeu[mt][nt], jeu[mt+1][nt]) = (jeu[mt+1][nt], jeu[mt][nt])
+		if (nt > 0) and (coup==2) :
+			nbEssais+=1
+			# on echange le trou et l'élément au dessus 
+			(jeu[mt][nt], jeu[mt][nt-1]) = (jeu[mt][nt-1], jeu[mt][nt])		
+		if (nt < dim-1) and (coup==3) :
+			nbEssais+=1
+			# on echange le trou et l'élément au dessus 
+			(jeu[mt][nt], jeu[mt][nt+1]) = (jeu[mt][nt+1], jeu[mt][nt])		
+
+jeu, ref = initTaquin("taquin4.txt")
+afficheJeu(jeu)
+afficheJeu(ref)
+
+jouer(ref, jeu)
+	
+
+
+
+
+
+
+
---
+++ Probleme-3/compteEstBonRec.py
@@ -0,0 +1,68 @@
+import random
+
+cpt=0
+
+def trouveExpr(v, valeurs) :
+    global cpt
+    cpt+=1
+    if len(valeurs) == 1 :     
+        if (v == valeurs[0]) : 
+            return (True,str(v))
+        else : 
+            return (False, "")
+    else :
+        if v in valeurs :
+            return (True, str(v))
+        else :
+            for x in valeurs :
+                valeurs2 = valeurs[:]
+                valeurs2.remove(x)
+
+                (t, ch) = trouveExpr(v+x, valeurs2)
+                if t: return (t, ch + " - " + str(x))
+
+                if (v >= x) : 
+                    (t, ch) = trouveExpr(v-x, valeurs2)
+                    if t : return (t, str(x)+" + (" + ch+") ")
+
+                if (v <= x) : 
+                    (t, ch) = trouveExpr(x-v, valeurs2)
+                    if t : return (t, str(x)+" + (" + ch+") ")
+                
+                if (v >= x) and v%x == 0: 
+                    (t, ch) = trouveExpr(v//x, valeurs2)
+                    if t : return (t, "("+ch+") * "+str(x))
+
+                if (v <= x) and x%v == 0: 
+                    (t, ch) = trouveExpr(x//v, valeurs2)
+                    if t : return (t, str(x)+" / ("+ch+") ")
+
+                (t, ch) = trouveExpr(v*x, valeurs2)
+                if t : return  (t, "("+ch+") / "+str(x))
+
+            return(False,"")
+
+NBNOMBRES = 6
+nombres=[]
+operateurs = ['+', '-', '*', '/']
+operandes=list(range(1,11))+list(range(1,11))+[25,50,75,100]
+#cible=841
+#nombres = [50,25,3,7,100,10]
+for i in range(NBNOMBRES) :
+    nombres.append(operandes[random.randint(0,len(operandes)-1)])
+cible = random.randint(100,999)
+
+res = trouveExpr (cible, nombres)
+print(cible, nombres, res, cpt)
+if (res[0]==False) :
+    for i in range(cible) :
+        print("écart",i)
+        res = trouveExpr (cible+i, nombres)
+        if (res[0]==True) : 
+            print(cible, cible+i, nombres, res, cpt)
+            break
+        res = trouveExpr (cible-i, nombres)
+        if (res[0]==True) : 
+            print(cible, cible-i, nombres, res, cpt)
+            break
+
